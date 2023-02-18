c89a24f84e69823931723eaea5d0648d237a0fbc
gaurang007 <gaurangghadiya99@gmail.com>
2023-02-18 11:14:22 +0530

    Updates

---
+++ precache-manifest.4cb1e7d3af5babe35d4859e3fc70bcc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68c793f92b8f2dec44139d3484750871",
+    "url": "/3sigma/index.html"
+  },
+  {
+    "revision": "704baece5550810e22b9",
+    "url": "/3sigma/static/js/2.7de5fc75.chunk.js"
+  },
+  {
+    "revision": "4957d44f39e8ba2e1b03536c856dc173",
+    "url": "/3sigma/static/js/2.7de5fc75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5f394cf303402cecba2",
+    "url": "/3sigma/static/js/main.18a5746a.chunk.js"
+  },
+  {
+    "revision": "147f0856d73ae3532a73",
+    "url": "/3sigma/static/js/runtime-main.e0ced7c4.js"
+  },
+  {
+    "revision": "4b236c6cb4c59d66b80dde6f9c614ebd",
+    "url": "/3sigma/static/media/Gilroy-Light.4b236c6c.ttf"
+  }
+]);
